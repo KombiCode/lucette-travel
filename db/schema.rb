a0c988fb1cade37e4d09ce0cd25e096472a61dae
Thierry Jet <phenix.tj38@gmail.com>
2021-03-15 17:35:13 +0100

    Create User model and Trip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_142159) do
+ActiveRecord::Schema.define(version: 2021_03_15_163254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.string "city"
+    t.date "begin_date"
+    t.date "end_date"
+    t.string "language"
+    t.string "devise"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +37,12 @@ ActiveRecord::Schema.define(version: 2021_03_15_142159) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trips", "users"
 end
