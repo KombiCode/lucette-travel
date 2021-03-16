5ae3aa1572cc63d4aacb7d7711d06ab0cf74ec83
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-16 20:10:13 +0100

    seeds partie 1 : transports, hôtels pour athènes et paros

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,42 +9,72 @@ User.destroy_all
 p "creating User"
 
 user1 = User.create(
-  firstname: "tutu",
-  lastname: "Choucroute",
+  firstname: "Louis",
+  lastname: "Dupont",
   phone: "0687676735",
-  email: "tutu@tutu.com",
+  email: "louis@gmail.com",
   password: "azerty"
   )
 
-p "done"
+p "#{User.count} users created"
 p "creating trip"
 
 trips1 = Trip.create(
-  name: "Greece, Athène",
+  name: "Greece",
   country: "Greece",
-  city: "Athene",
-  begin_date: "01/02/2021",
-  end_date: "02/02/2021",
-  language: "en",
+  city: "Athens",
+  begin_date: "24/03/2021",
+  end_date: "02/04/2021",
+  language: "Greek - English",
   devise: "€",
-  user: User.find_by(email: "tutu@tutu.com")
+  user: User.find_by(email: "louis@gmail.com")
   )
 
-p "done"
+p "#{Trip.count} trips created"
+
 p "creating to-do"
 
 to_dos1 = ToDo.create(
-  name: "shower",
-  description: "Take a shower before leaving",
-  done: false,
-  trip: Trip.find_by(name: "Greece, Athène")
+  name: "Visa",
+  description: "Check if you need a visa",
+  done: true,
+  trip: Trip.find_by(name: "Greece")
   )
 
-p "done"
+to_dos2 = ToDo.create(
+  name: "Passport",
+  description: "Take it for both of us",
+  done: true,
+  trip: Trip.find_by(name: "Greece")
+  )
+
+to_dos3 = ToDo.create(
+  name: "Souvenirs",
+  description: "Bring Back souvenirs",
+  done: false,
+  trip: Trip.find_by(name: "Greece")
+  )
+
+to_dos4 = ToDo.create(
+  name: "Bath suit",
+  description: "Buy a bath suit for Paros",
+  done: false,
+  trip: Trip.find_by(name: "Greece")
+  )
+
+to_dos5 = ToDo.create(
+  name: "Luggage",
+  description: "Smartphone charger, glasses, tablet, headphones",
+  done: true,
+  trip: Trip.find_by(name: "Greece")
+  )
+
+p "#{ToDo.count} todos created"
 p "creating Activity"
 
 activity1 = Activity.create(
-  name: "Visit Parthenon",
+  category: "",
+  name: "Parthenon",
   description: "Nice place to stay and a good view of the city",
   date: "01/02/2021",
   price: 45,
@@ -54,20 +84,289 @@ activity1 = Activity.create(
   )
 
 
-Trip.find_by(name: "Greece, Athène").activities << activity1
-
-
-p "done"
-p "creating booking"
-
-booking1 = Booking.create(
-  category: "Hotel",
-  name: "Beluga",
-  begin_date: "03/02/2021",
-  end_date: "04/02/2021",
-  price: 22,
-  address: "rue du fion",
-  trip: Trip.find_by(name: "Greece, Athène")
+activity2 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
   )
 
-p "done"
+activity3 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity4 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity5 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity6 = Activity.create(
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity7 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity8 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity9 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity10 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity11 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity12 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity13 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity14 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity15 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity16 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity17 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity18 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity19 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+activity20 = Activity.create(
+  category: "",
+  name: "The Acropolis",
+  description: "Nice place to stay and a good view of the city",
+  date: "01/02/2021",
+  price: 45,
+  rating: 3,
+  done: false,
+  address: "8 Rue du foie"
+  )
+
+Trip.find_by(name: "Greece").activities << activity1
+
+p "#{Activity.count} activities created"
+
+p "creating booking"
+
+hotel1 = Booking.create(
+  category: "Hotel",
+  name: "B4B Athens Signature Hotel",
+  begin_date: "24/03/2021",
+  end_date: "26/03/2021",
+  price: 152,
+  address: " Theofilopoulou 18, Kallirois Avenue, 11743, Athenes, 11743, Greece",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+flat1 = Booking.create(
+  category: "Flat",
+  name: "Marcora's Historical Estate ",
+  begin_date: "26/03/2021",
+  end_date: "01/04/2021",
+  price: 480,
+  address: "Eparchiaki Odos Pontis-Messogis, Strongylí, 49084, Greece",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+hotel2 = Booking.create(
+  category: "Hotel",
+  name: "Acropolis View Hotel",
+  begin_date: "01/04/2021",
+  end_date: "02/04/2021",
+  price: 74,
+  address: "Webster 10 & Robertou Galli, Athenes, 11742, Greece",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+plane1 = Booking.create(
+  category: "Plane",
+  name: "KLM",
+  begin_date: "24/03/2021, 11h50",
+  end_date: "25/03/2021, 1h05",
+  price: 263.40,
+  address: "Lyon-Saint Exupéry Airport (LYS), Colombier-Saugnieu",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+plane2 = Booking.create(
+  category: "Plane",
+  name: "KLM",
+  begin_date: "02/04/2021, 06h00",
+  end_date: "02/04/2021, 10h55",
+  price: 233.40,
+  address: "Athenes Elefthérios-Venizélos International Airport (ATH), Attiki Odos, Spata-Artémis, Grèce",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+ferry1 = Booking.create(
+  category: "Ferry",
+  name: "Blue Star Ferries",
+  begin_date: "26/03/2021, 17h30",
+  end_date: "26/03/2021, 21h45",
+  price: 60,
+  address: "Port of Piraeus, Pirée, Greece",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+ferry2 = Booking.create(
+  category: "Ferry",
+  name: "Blue Star Ferries",
+  begin_date: "01/04/2021, 10h45",
+  end_date: "01/04/2021, 15h00",
+  price: 65,
+  address: "Paros 844 00, Greece",
+  trip: Trip.find_by(name: "Greece")
+  )
+
+p "#{Booking.count} bookings created"
+p "finish"
