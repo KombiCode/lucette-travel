5aaea52d62328ee0962049a0258327ebcc2dea8b
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-17 14:15:44 +0100

    seeds1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ ToDo.destroy_all
 Trip.destroy_all
 User.destroy_all
 
-
+# CATEGORIES ["Points of interest & Lookouts", "Flea & Street Markets" "Beaches", Museums", "Historic sites", "Neighborhoods", "Gardens", "Marinas", "Churches", "Wineyards"]
 p "creating User"
 
 user1 = User.create(
@@ -26,6 +26,17 @@ trips1 = Trip.create(
   begin_date: "24/03/2021",
   end_date: "02/04/2021",
   language: "Greek - English",
+  devise: "€",
+  user: User.find_by(email: "louis@gmail.com")
+  )
+
+trips2 = Trip.create(
+  name: "Island",
+  country: "Island",
+  city: "Reykjavík",
+  begin_date: "15/01/2020",
+  end_date: "25/01/2021",
+  language: "English",
   devise: "€",
   user: User.find_by(email: "louis@gmail.com")
   )
@@ -73,224 +84,330 @@ p "#{ToDo.count} todos created"
 p "creating Activity"
 
 activity1 = Activity.create(
-  category: "",
+  category: "Historic site",
   name: "Parthenon",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  description: "The majestic ruins of an ancient Greek building, known for its dignified white marble columns and perfect sense of proportion.",
+  date: "",
+  duration: "> 3h",
+  Opening_hours: "Sun Sat 8.00am - 8.00pm",
+  price: "",
+  rating: 4,5,
   done: false,
-  address: "8 Rue du foie"
+  address: "Acropolis Top of Dionyssiou Areopagitou, Athens 105 58 Greece"
   )
 
 
 activity2 = Activity.create(
-  category: "",
+  category: "Historic site",
   name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  description: "The most famous site in all of Athens, this large hill lies in the center of the city and contains a cluster of ancient ruins.",
+  date: "",
+  duration: "1h",
+  Opening_hours: "all day",
+  price: 13,
+  rating: 4.5,
   done: false,
-  address: "8 Rue du foie"
+  address: "via Dionysiou Areopagitou str., Athens 105 58 Greece"
+  )
+
+activity2bis = Activity.create(
+  category: "Museum",
+  name: "Acropolis Museum",
+  description: "Modern, uptodate building on the foot of the Acropolis housing important ancient Greek statues and much more.",
+  date: "",
+  duration: "2/3h",
+  Opening_hours: "",
+  price: 7.50,
+  rating: 4.5,
+  done: false,
+  address: "Dionysiou Areopagitou 15, Athens 117 42 Greece"
   )
 
 activity3 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  category: "Museum",
+  name: "Museum of Cycladic Art",
+  description: "A dynamic cultural institution in the centre of Athens, the Museum of Cycladic Art focuses on promoting the ancient cultures of the Aegean and Cyprus, with particular emphasis on Cycladic art of the third millennium BC. The Museum’s permanent collections include 3000 Cycladic, ancient Greek, and ancient Cypriot artefacts, witnesses to the cultures that flourished in the Aegean and the Eastern Mediterranean from the fourth millennium BC to approximately the sixth century AD. The Museum of Cycladic Art’s temporary exhibitions focus on archaeology and modern and contemporary art with the aim to familiarize the public with important twentieth and twenty-first century artists and to explore the relations between ancient cultures and modern art. Salvador Dali, Pablo Picasso, Thomas Struth, Louise Bourgeois, Sarah Lucas, Ugo Rondinone, Jannis Kounellis, Mario Merz, Ai Weiwei, and Cy Twombly are among the artists hosted.",
+  date: "",
+  duration: "1/2h",
+  Opening_hours: "Sun 11.00am - 5.00pm Mon Wed-Sat 10.00am - 5.00pm",
+  price: 7,
+  rating: 4,
   done: false,
-  address: "8 Rue du foie"
+  address: "4 Neofytou Douka str., Athens 106 74 Greece"
   )
 
 activity4 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  category: "Neighbourhood",
+  name: "Monastiraki Neighbourhood",
+  description: "Lively flea market neighborhood with lots of small tavernas serving good-value local snacks.",
+  date: "",
+  duration: "2/3h",
+  Opening_hours: "all day",
+  price: 0,
+  rating: 4,
   done: false,
-  address: "8 Rue du foie"
+  address: "Monastikari Square"
   )
 
-activity5 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity5 = Activity.create(
+#   category: "",
+#   name: "Varvakios Agora",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity6 = Activity.create(
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity6 = Activity.create(
+#   name: "The Acropolis",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
 activity7 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  category: "Points of interest & Lookouts",
+  name: "Lycabettus Hill",
+  description: "Walk or take the funicular tramway to the top of this hill for the best views of Athens",
+  date: "",
+  duration: "1/2h",
+  Opening_hours: "All day",
+  price: 0,
+  rating: 4.5,
   done: false,
-  address: "8 Rue du foie"
+  address: "Mount Lycabettus, Athens 114 71, Greece"
   )
 
 activity8 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  category: "Neighbourhood",
+  name: "Plaka Neighbourhood",
+  description: "Historic neighborhood located at the base of the hill topped by the Acropolis.",
+  date: "",
+  duration: "2/3h",
+  Opening_hours: "All day",
+  price: 0,
+  rating: 4.5,
   done: false,
-  address: "8 Rue du foie"
+  address: "Athens 10556 Greece"
   )
 
-activity9 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity9 = Activity.create(
+#   category: "",
+#   name: "Temple of Poseidon",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity10 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity10 = Activity.create(
+#   category: "",
+#   name: "The National Museum of Contemporary Art in Athens",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity11 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity11 = Activity.create(
+#   category: "",
+#   name: "Erechtheion",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
 activity12 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  category: "Museum",
+  name: "National Archaeological Museum",
+  description: "The National Archaeological Museum in Athens houses some of the most important artifacts from a variety of archaeological locations around Greece from prehistory to late antiquity. It is considered one of the greatest museums in the world and contains the richest collection of artifacts from Greek antiquity worldwide.",
+  date: "",
+  duration: "2/3h",
+  Opening_hours: "From April until October 31st: Tu 1.00pm - 8.00pm Wed-Mon 8.00am - 8.00pm",
+  price: 12,
+  rating: 4.5,
   done: false,
-  address: "8 Rue du foie"
-  )
+  address: "28is Oktovriou 44, Athina 106 82, Greece"
+#   )
 
-activity13 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity13 = Activity.create(
+#   category: "",
+#   name: "Temple of Hephaestus",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity14 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity14 = Activity.create(
+#   category: "",
+#   name: "Museum of Cycladic Art",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity15 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity15 = Activity.create(
+#   category: "",
+#   name: "Temple of Athena Nike",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity16 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity16 = Activity.create(
+#   category: "",
+#   name: "Temple of Olympian Zeus",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity17 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity17 = Activity.create(
+#   category: "",
+#   name: "Ancient Agora of Athens",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
 activity18 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
+  category: "Historic sites",
+  name: "Panathenaic Stadium",
+  description: "The Old Olympic Stadium in Athens Greece, built of granite in late 19th century on the site of ancient Athens from the 4th Century; hosted the first modern games in 1896.",
+  date: "",
+  duration: "1h",
+  Opening_hours: "Sun - Sat 8.00am - 7.00pm",
+  price: 0,
+  rating: 4.5,
   done: false,
-  address: "8 Rue du foie"
+  address: "Vassileos Konstantinou Avenue opposite the statue of Myron Discobolus, Athens 116 35 Greece"
   )
 
-activity19 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity19 = Activity.create(
+#   category: "",
+#   name: "Odeon of Herodes Atticus",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
-activity20 = Activity.create(
-  category: "",
-  name: "The Acropolis",
-  description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
-  price: 45,
-  rating: 3,
-  done: false,
-  address: "8 Rue du foie"
-  )
+# activity20 = Activity.create(
+#   category: "",
+#   name: "Philopappos Monument",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity21 = Activity.create(
+#   category: "",
+#   name: "Philopappos Monument",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity22 = Activity.create(
+#   category: "",
+#   name: "Byzantine and Christian Museum",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity23 = Activity.create(
+#   category: "",
+#   name: "Kapnikarea",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity23 = Activity.create(
+#   category: "",
+#   name: "Kapnikarea",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity24 = Activity.create(
+#   category: "",
+#   name: "National Garden",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity25 = Activity.create(
+#   category: "",
+#   name: "Theatre of Dionysus",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
+
+# activity26 = Activity.create(
+#   category: "",
+#   name: "Aeropagus",
+#   description: "Nice place to stay and a good view of the city",
+#   date: "01/02/2021",
+#   price: 45,
+#   rating: 3,
+#   done: false,
+#   address: "8 Rue du foie"
+#   )
 
 Trip.find_by(name: "Greece").activities << activity1
 
@@ -370,3 +487,47 @@ ferry2 = Booking.create(
 
 p "#{Booking.count} bookings created"
 p "finish"
+
+
+
+
+
+
+100.times do
+Activity.create(
+  category: "",
+  name: Faker::Name.name,
+  description: "Nice place to stay and a good view of the city",
+  date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+  price: 45,
+  rating: rand(1..5),
+  done: [true, false].sample,
+  address: "8 Rue du foie"
+  )
+
+end
+
+
+# booking = Booking.where(user_id: User.find(8));
+
+# thierry_activites = booking
+#               .activities
+#               .where(rating: 3..5)
+#               .where(date: Date.current.beginning_of_month..Date.current.end_of_month)
+#               where(number_of_people_max: 2)
+
+
+# activities = current_user
+#                 .activities
+#                 .where(date: Date.current.beginning_of_month..Date.current.end_of_month)
+
+
+# commun_activities = activities.pluck(:id) - thierry_activites.pluck(:id)
+
+# commun_activities.each do |activity|
+#   PushNotification.perform_later(activity)
+# end
+
+
+
+
