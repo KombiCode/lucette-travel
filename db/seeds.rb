838ee64c7bad7b5fc6fb942168536d89b650b20c
Thierry Jet <phenix.tj38@gmail.com>
2021-03-17 22:49:34 +0100

    fix seeds because DB has changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -87,7 +87,6 @@ activity1 = Activity.create(
   category: "Historic site",
   name: "Parthenon",
   description: "The majestic ruins of an ancient Greek building, known for its dignified white marble columns and perfect sense of proportion.",
-  date: "",
   duration: "> 3h",
   opening_hours: "Sun Sat 8.00am - 8.00pm",
   price: "",
@@ -100,7 +99,6 @@ activity2 = Activity.create(
   category: "Historic site",
   name: "The Acropolis",
   description: "The most famous site in all of Athens, this large hill lies in the center of the city and contains a cluster of ancient ruins.",
-  date: "",
   duration: "1h",
   opening_hours: "all day",
   price: 13,
@@ -112,7 +110,6 @@ activity2bis = Activity.create(
   category: "Museum",
   name: "Acropolis Museum",
   description: "Modern, uptodate building on the foot of the Acropolis housing important ancient Greek statues and much more.",
-  date: "",
   duration: "2/3h",
   opening_hours: "",
   price: 7.50,
@@ -124,7 +121,6 @@ activity3 = Activity.create(
   category: "Museum",
   name: "Museum of Cycladic Art",
   description: "A dynamic cultural institution in the centre of Athens, the Museum of Cycladic Art focuses on promoting the ancient cultures of the Aegean and Cyprus, with particular emphasis on Cycladic art of the third millennium BC. The Museum’s permanent collections include 3000 Cycladic, ancient Greek, and ancient Cypriot artefacts, witnesses to the cultures that flourished in the Aegean and the Eastern Mediterranean from the fourth millennium BC to approximately the sixth century AD. The Museum of Cycladic Art’s temporary exhibitions focus on archaeology and modern and contemporary art with the aim to familiarize the public with important twentieth and twenty-first century artists and to explore the relations between ancient cultures and modern art. Salvador Dali, Pablo Picasso, Thomas Struth, Louise Bourgeois, Sarah Lucas, Ugo Rondinone, Jannis Kounellis, Mario Merz, Ai Weiwei, and Cy Twombly are among the artists hosted.",
-  date: "",
   duration: "1/2h",
   opening_hours: "Sun 11.00am - 5.00pm Mon Wed-Sat 10.00am - 5.00pm",
   price: 7,
@@ -136,7 +132,6 @@ activity4 = Activity.create(
   category: "Neighbourhood",
   name: "Monastiraki Neighbourhood",
   description: "Lively flea market neighborhood with lots of small tavernas serving good-value local snacks.",
-  date: "",
   duration: "2/3h",
   opening_hours: "all day",
   price: 0,
@@ -148,7 +143,6 @@ activity5 = Activity.create(
   category: "",
   name: "Varvakios Agora",
   description: "Nice place to stay and a good view of the city",
-  date: "01/02/2021",
   price: 45,
   rating: 3,
   address: "8 Rue du foie"
@@ -157,7 +151,6 @@ activity5 = Activity.create(
 # activity6 = Activity.create(
 #   name: "The Acropolis",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -167,7 +160,6 @@ activity7 = Activity.create(
   category: "Points of interest & Lookouts",
   name: "Lycabettus Hill",
   description: "Walk or take the funicular tramway to the top of this hill for the best views of Athens",
-  date: "",
   duration: "1/2h",
   opening_hours: "All day",
   price: 0,
@@ -179,7 +171,6 @@ activity8 = Activity.create(
   category: "Neighbourhood",
   name: "Plaka Neighbourhood",
   description: "Historic neighborhood located at the base of the hill topped by the Acropolis.",
-  date: "",
   duration: "2/3h",
   opening_hours: "All day",
   price: 0,
@@ -191,7 +182,6 @@ activity8 = Activity.create(
 #   category: "",
 #   name: "Temple of Poseidon",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -201,7 +191,6 @@ activity8 = Activity.create(
 #   category: "",
 #   name: "The National Museum of Contemporary Art in Athens",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -211,7 +200,6 @@ activity8 = Activity.create(
 #   category: "",
 #   name: "Erechtheion",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -221,7 +209,6 @@ activity12 = Activity.create(
   category: "Museum",
   name: "National Archaeological Museum",
   description: "The National Archaeological Museum in Athens houses some of the most important artifacts from a variety of archaeological locations around Greece from prehistory to late antiquity. It is considered one of the greatest museums in the world and contains the richest collection of artifacts from Greek antiquity worldwide.",
-  date: "",
   duration: "2/3h",
   opening_hours: "From April until October 31st: Tu 1.00pm - 8.00pm Wed-Mon 8.00am - 8.00pm",
   price: 12,
@@ -233,7 +220,6 @@ activity13 = Activity.create(
   category: "Museum",
   name: "Principal",
   description: "Nice place to stay and a good view of the city",
-  date: "17/03/2021",
   duration: "2/3h",
   opening_hours: "From April until October 31st: Tu 1.00pm - 8.00pm Wed-Mon 8.00am - 8.00pm",
   price: 45,
@@ -245,7 +231,6 @@ activity14 = Activity.create(
   category: "Museum",
   name: "Principal 2",
   description: "Nice place to stay and a good view of the city",
-  date: "18/03/2021",
   duration: "2/3h",
   opening_hours: "From April until October 31st: Tu 1.00pm - 8.00pm Wed-Mon 8.00am - 8.00pm",
   price: 45,
@@ -257,7 +242,6 @@ activity14 = Activity.create(
 #   category: "",
 #   name: "Temple of Athena Nike",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -267,7 +251,6 @@ activity14 = Activity.create(
 #   category: "",
 #   name: "Temple of Olympian Zeus",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -277,7 +260,6 @@ activity14 = Activity.create(
 #   category: "",
 #   name: "Ancient Agora of Athens",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -287,7 +269,6 @@ activity18 = Activity.create(
   category: "Historic sites",
   name: "Panathenaic Stadium",
   description: "The Old Olympic Stadium in Athens Greece, built of granite in late 19th century on the site of ancient Athens from the 4th Century; hosted the first modern games in 1896.",
-  date: "",
   duration: "1h",
   opening_hours: "Sun - Sat 8.00am - 7.00pm",
   price: 0,
@@ -299,7 +280,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Odeon of Herodes Atticus",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -309,7 +289,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Philopappos Monument",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -319,7 +298,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Philopappos Monument",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -329,7 +307,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Byzantine and Christian Museum",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -339,7 +316,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Kapnikarea",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -349,7 +325,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Kapnikarea",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -359,7 +334,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "National Garden",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -369,7 +343,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Theatre of Dionysus",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -379,7 +352,6 @@ activity18 = Activity.create(
 #   category: "",
 #   name: "Aeropagus",
 #   description: "Nice place to stay and a good view of the city",
-#   date: "01/02/2021",
 #   price: 45,
 #   rating: 3,
 #   address: "8 Rue du foie"
@@ -476,7 +448,7 @@ Activity.create(
   category: "",
   name: Faker::Name.name,
   description: "Nice place to stay and a good view of the city",
-  date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+  # date: Faker::Date.between(from: 2.days.ago, to: Date.today),
   price: 45,
   rating: rand(1..5),
   address: "8 Rue du foie"
