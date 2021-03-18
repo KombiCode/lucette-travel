2b9a521f53c30e1e3dd50d359e0c0417d9f9d43e
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-03-18 15:54:27 +0100

    faster the webpack-dev-server

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import "stylesheets/tailwind"
 import "stylesheets/application"
 import "controllers"
 import 'swiper/swiper-bundle.min.css'
