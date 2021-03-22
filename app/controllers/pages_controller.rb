37d654a84be0174d62c80c87dc32180a1539d6ec
Thierry Jet <phenix.tj38@gmail.com>
2021-03-22 11:17:02 +0100

    prepare stuff for fake booking notif

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
 
   def home
     @trips = Trip.all
+    respond_to do |format|
+      format.html
+      # TODO put in the json info we need
+      # For now just a test (not yet displayed anyway)
+      format.json { render json: { bookings: Booking.all } }
+    end
+    
   end
 
   def index
