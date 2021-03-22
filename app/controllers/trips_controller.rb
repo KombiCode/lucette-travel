a8fc5ba24b2b8c0908fdcea45980f136dcb31550
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-22 03:55:37 +0100

    ajout new et create dans trip controller+ route + view ajout formulaire

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,33 @@
 class TripsController < ApplicationController
+  before_action :find_trip, only: [:show, :create]
+
   def index
     @trips = Trip.all
   end
 
   def show
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_trip
     @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name, :country, :city, :begin_date, :end_date, :language, :devise, :description, :photo_title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [ :show, :create, :edit, :update , :destroy ]
-  resources :trips, only: [ :index, :show ] do
+  resources :trips, only: [ :index, :show, :new, :create ] do
     # resources :trip_activities, only: [ :new, :create, :index ]
     resources :bookings, only: [ :new, :create, :index, :show ]
     resources :tasks, only: [ :new, :create, :index, :show ]
