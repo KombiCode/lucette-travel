ad864b4401b71acf84cd5785ee67da36c2dd11e0
Thierry Jet <phenix.tj38@gmail.com>
2021-03-22 21:54:27 +0100

    Manage marker by displayed activity on the map. Also improve time_select.

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,7 +2,10 @@ import mapboxgl from '!mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 let map;
-let marker;
+let markers;
+let bounds;
+
+let mapMarkers = []
 
 const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -12,44 +15,45 @@ const buildMap = (mapElement) => {
   });
 };
 
-const showActivity = (activityId) => {
-  console.log("map", map)
-  console.log("activityId", activityId)
+const addActivityMarkerToMap = (activityId) => {
+  const activity_marker = markers.find(item => item["id"] == activityId);
+  const popup = new mapboxgl.Popup().setHTML(activity_marker.infoWindow);
+  // Create a HTML element for your custom marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${activity_marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '25px';
+  element.style.height = '25px';
+  // Pass the element as an argument to the new marker
+  const map_marker = new mapboxgl.Marker(element);
+  map_marker.setLngLat([activity_marker.lng, activity_marker.lat])
+  map_marker.setPopup(popup)
+  map_marker.addTo(map);
+  // Fit
+  bounds.extend([ activity_marker.lng, activity_marker.lat ])
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  // Add to array
+  mapMarkers.push(map_marker)
 }
 
-const addMarkersToMap = (markers) => {
-  markers.forEach((marker) => {
-  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-  // Create a HTML element for your custom marker
-    const element = document.createElement('div');
-    element.className = 'marker';
-    element.style.backgroundImage = `url('${marker.image_url}')`;
-    element.style.backgroundSize = 'contain';
-    element.style.width = '25px';
-    element.style.height = '25px';
-    // Pass the element as an argument to the new marker
-    new mapboxgl.Marker(element)
-      .setLngLat([marker.lng, marker.lat])
-      .setPopup(popup)
-      .addTo(map);
-  });
-};
-
-const fitMapToMarkers = (markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-};
+const showActivity = (activityId) => {
+  // removing
+  mapMarkers.forEach((marker) => marker.remove())
+  mapMarkers = []
+  // adding the one for the activity
+  addActivityMarkerToMap(activityId);
+}
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
   if (mapElement) {
     map = buildMap(mapElement);
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(markers);
-    fitMapToMarkers(markers);
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }));
+    markers = JSON.parse(mapElement.dataset.markers);
+    bounds = new mapboxgl.LngLatBounds()
+    // No more search as we display a small map only to see current activity loacation
+    // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+    //   mapboxgl: mapboxgl }));
   }
 };
 
