e7a3e8653f8a3f7a38250afcda7b04ede4a573e8
Thierry Jet <phenix.tj38@gmail.com>
2021-03-24 08:56:28 +0100

    Improve bell notif stuff. Prepare for onClick

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,18 +14,21 @@ class PagesController < ApplicationController
 
       @last_booking_name = Booking.count == 0 ? "" : Booking.last.name
       @ghost_activity = Activity.where("name = ?", 'ghost').where("category = ?", 'fake')
+      @notif_message = ""
 
       if (@last_booking_name == "Flight LYS - KEF to store")
+        @notif_message = "Your booking flight for upcoming Iceland trip has been added"
         format.json { render json: {
                         notifType: 'newBooking',
                         newBooking: @last_booking_name,
-                        notifMessage: 'Your booking flight for upcoming Iceland trip has been added'
+                        notifMessage: @notif_message
                       }
                     }
       elsif @ghost_activity.any?
+        @notif_message = "You have no activities planned for tomorrow, pick up one !"
         format.json { render json: {
                         notifType: 'emptyActivities',
-                        notifMessage: "You have no activities planned for tomorrow, pick up one !"
+                        notifMessage: @notif_message
                       }
                     }
       else
