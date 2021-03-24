8a4359e699ee49c9f74f60b67209d9dbe405a280
Thierry Jet <phenix.tj38@gmail.com>
2021-03-24 08:17:59 +0100

    improve notifs

--- app/javascript/controllers/fakenotif_controller.js
+++ app/javascript/controllers/fakenotif_controller.js
@@ -13,9 +13,13 @@ export default class extends Controller {
       .then((data) => {
         // TODO put what we want to display here  
         //this.countTarget.innerText = data.bookings.length;
-        const new_booking_name = data.newbooking;
+        const notifType = data.notifType;
+        const notifMessage = data.notifMessage;
+        const bookingName = data.newBooking;
         let bell_html = "<i class=\"far fa-bell text-gray text-2xl text-gray-600 mx-4\"></i>"
-        if (new_booking_name == "Flight LYS - KEF to store") {
+        if (notifType == 'emptyActivities') {
+          bell_html = "<span style=\"color: Tomato;\"><i class=\"fas fa-bell text-red text-2xl text-red-600 mx-4\"></i></span>";
+        } else if (notifType == 'newBooking') {
           bell_html = "<span style=\"color: Dodgerblue;\"><i class=\"fas fa-bell text-blue text-2xl text-blue-600 mx-4\"></i></span>";
         }
         this.countTarget.innerHTML = bell_html;
