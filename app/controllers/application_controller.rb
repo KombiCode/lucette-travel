86d81022dea0b1aab35989fb1c1b2db479df2880
Thierry Jet <phenix.tj38@gmail.com>
2021-04-04 22:24:41 +0200

    Always use user 'elodie' and act as if was logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
+# Use a 'global variable' to set the user we want 
+$elodie_user = User.find_by(email: "elodied@lucette-travel.com")
+
 class ApplicationController < ActionController::Base
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
